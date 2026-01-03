8b879261a92a0f18f26bb62987bde9e6e0994748
Lucas Brasil Silva <lucasbrasil396@gamil.com>
2026-01-02 23:21:15 -0300

    Service layer, component registration

--- springboot/src/main/java/br/com/estudos/springboot/controller/UserController.java
+++ springboot/src/main/java/br/com/estudos/springboot/controller/UserController.java
@@ -1,19 +1,33 @@
 package br.com.estudos.springboot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.estudos.springboot.model.User;
+import br.com.estudos.springboot.services.UserService;
 
 @RestController
 @RequestMapping(value = "/Users")
 public class UserController {
+
+    @Autowired
+    private UserService service;
     
     @GetMapping
-    public ResponseEntity<User> finndAll() {
-        User newUser = new User(1L, "Maria", "maria@gmail.com", "99999999", "123456");
-        return ResponseEntity.ok().body(newUser);
+    public ResponseEntity<List<User>> finndAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
