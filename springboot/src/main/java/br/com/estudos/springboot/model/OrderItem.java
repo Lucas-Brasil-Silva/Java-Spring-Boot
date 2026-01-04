d94107217168fec5d7f7d9406714a176799a6d1e
Lucas Brasil Silva <lucasbrasil396@gamil.com>
2026-01-04 19:47:12 -0300

    Product-OrderItem one-to-many association

--- springboot/src/main/java/br/com/estudos/springboot/model/OrderItem.java
+++ springboot/src/main/java/br/com/estudos/springboot/model/OrderItem.java
@@ -54,7 +54,7 @@ public class OrderItem implements Serializable{
         id.setOrder(order);
     }
 
-     public Product getProduct() {
+    public Product getProduct() {
         return id.getProduct();
     }
 
